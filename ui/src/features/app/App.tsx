2fab8fb50c2c8bd83ad3ffb01fb669d56966dde0
Thouis (Ray) Jones <thouis@gmail.com>
2026-07-08 11:24:56 -0400

    PR9b: version badge

--- ui/src/features/app/App.tsx
+++ ui/src/features/app/App.tsx
@@ -25,7 +25,7 @@ const VERSION_BADGE: React.CSSProperties = {
 function App() {
   return (
     <>
-      <span style={VERSION_BADGE}>pr9</span>
+      <span style={VERSION_BADGE}>pr9b</span>
       {devMode() && (
         <Helmet>
           <link rel="icon" href={`${process.env.PUBLIC_URL}/favicon-dev.ico`} />
